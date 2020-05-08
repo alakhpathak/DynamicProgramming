e1654f974d0aef66c369e8cf57395c95176ba694
Alakh Pathak <alakh1989@gmail.com>
2020-05-08 22:26:05 +0530

    mutliple dp program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice.PrintCombination("AABC");
+        }
+    }
+}
